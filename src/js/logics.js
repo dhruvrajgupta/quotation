ad7f708c4bfd1c6df3f1a2e095798b4e7a5bf7cb
Dhruv Raj <dhruv.learner@gmail.com>
2022-11-24 03:07:05 +0530

    edit buyer details show on page

--- src/js/logics.js
+++ src/js/logics.js
@@ -5,3 +5,31 @@ $(function () {
 		$(this).load(file);
 	})
 });
+
+var company = {
+    "name": "Yash Corporation",
+    "address1": "26, Chatta Wala Gulli,",
+    "address2": "Kolkata - 700-012, West Bengal",
+    "phone": "(+91) 9433099062",
+    "email": "yashcorpkol@gmail.com",
+    "gstin": ""
+};
+
+var buyerDetails = {}
+
+function showBuyerDetails(buyerDetails){
+    $("#textBuyerName").text(buyerDetails["name"]);
+    $("#textBuyerAddress1").text(buyerDetails["address1"]);
+    $("#textBuyerAddress2").text(buyerDetails["address2"]);
+    $("#textBuyerPhone").text(buyerDetails["phone"]);
+}
+
+$(document).on('click', '#saveBtnBuyerDetail', function(){ 
+    // Your Code
+    buyerDetails["name"] = $("#buyer_name").val();
+    buyerDetails["address1"] = $("#buyer_address1").val();
+    buyerDetails["address2"] = $("#buyer_address2").val();
+    buyerDetails["phone"] = $("#buyer_phone").val();
+
+    showBuyerDetails(buyerDetails);
+});
